b5d525373882a17b859e0675b3fc47379d4c7f2b
Joan Mariné <joan.marine@dribba.com>
2024-04-03 12:53:00 +0200

    Fix home header size and icons

--- lib/modules/statments/components/custom_header.dart
+++ lib/modules/statments/components/custom_header.dart
@@ -1,6 +1,11 @@
+import 'dart:math';
+
 import 'package:dui/dui.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:palumba_eu/utils/common_ui/app_dimens.dart';
+import 'package:palumba_eu/utils/managers/i18n_manager/translations/generated/l10n.dart';
 import 'package:path_drawing/path_drawing.dart';
 
 class CustomHeader extends StatelessWidget {
@@ -21,31 +26,53 @@ class CustomHeader extends StatelessWidget {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Container(
-              height: 40,
-              width: 32,
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(.3),
-                borderRadius:
-                    BorderRadius.circular(DUI.spacing.largeBorderRadius),
+            Transform.rotate(
+              angle: -pi * .06,
+              child: Container(
+                height: 32,
+                width: 40,
+                margin:
+                    EdgeInsets.only(top: DUI.spacing.smallPaddingValue * 1.5),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(.3),
+                  borderRadius:
+                      BorderRadius.circular(DUI.spacing.largeBorderRadius),
+                ),
+                child: Transform.rotate(
+                  angle: pi * .06,
+                  child: Padding(
+                      padding: EdgeInsets.all(8),
+                      child:
+                          SvgPicture.asset('assets/images/ic_arrow_back.svg')),
+                ),
               ),
-              child: Icon(Icons.arrow_back),
             ),
-            DUI.text.title1(context, 'Palumba',
+            DUI.text.title1(context, S.of(context).shortAppName,
                 color: Theme.of(context).colorScheme.primary),
-            Container(
-              height: 40,
-              width: 32,
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(.3),
-                borderRadius:
-                    BorderRadius.circular(DUI.spacing.largeBorderRadius),
+            Transform.rotate(
+              angle: pi * .06,
+              child: Container(
+                margin:
+                    EdgeInsets.only(top: DUI.spacing.smallPaddingValue * 1.5),
+                height: 32,
+                width: 40,
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(.3),
+                  borderRadius:
+                      BorderRadius.circular(DUI.spacing.largeBorderRadius),
+                ),
+                child: Transform.rotate(
+                  angle: pi * -.06,
+                  child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: SvgPicture.asset('assets/images/ic_home.svg')),
+                ),
               ),
-              child: Icon(Icons.house_outlined),
             ),
           ],
         ),
-        DUI.spacing.spacer(multiplier: 2),
+        DUI.spacing.spacer(),
+        DUI.spacing.spacer(small: true),
         SizedBox(
           width: double.infinity,
           child: CustomPaint(painter: _CustomHeaderPainter()),
@@ -73,7 +100,7 @@ class _CustomHeaderPainter extends CustomPainter {
 
     path.moveTo(0, 10);
     path.quadraticBezierTo(size.width * .5, -20, size.width, 10);
-    var curvePath = dashPath(path, dashArray: CircularIntervalList([8, 5]));
+    var curvePath = dashPath(path, dashArray: CircularIntervalList([6, 3]));
     canvas.drawPath(curvePath, paint);
   }
 
