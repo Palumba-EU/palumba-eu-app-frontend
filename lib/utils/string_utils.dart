6d7fdecfaf4b2c189a948f97f1299671c29c1454
Michael Kudler <michael.kudler@aon.at>
2024-05-08 21:42:19 +0200

    Update data protection URL

--- lib/utils/string_utils.dart
+++ lib/utils/string_utils.dart
@@ -8,7 +8,7 @@ class StringUtils {
   static String electionsUrl(String langCode) =>
       'https://elections.europa.eu/$langCode/use-your-vote/';
   static String organizationUrl = 'https://palumba.org/';
-  static String privacyStatementUrl = 'https://palumba.org/privacy-statement/';
+  static String privacyStatementUrl = 'https://palumba.eu/data-protection/';
   static String webUrl = 'https://palumba.eu';
   static String faqUrl = 'https://www.palumba.eu/faq';
   static String rickrollUrl = 'https://www.youtube.com/watch?v=dQw4w9WgXcQg';
