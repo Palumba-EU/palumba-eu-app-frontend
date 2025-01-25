9cbb34e3d3a713cd953728ade76e3dea70e58e65
Michael Poppinger <michael@codeandcomments.at>
2025-01-25 13:17:31 +0100

    update translation generation script

--- scripts/generate_intl_resources.sh
+++ scripts/generate_intl_resources.sh
@@ -8,27 +8,27 @@ ZIP_SOURCE="$1"
 ZIP_DEST_DIRECTORY=$(dirname "$ZIP_SOURCE")"/tmp"
 
 ARB_DEST_DIR=$(grep 'arb_dir:' pubspec.yaml | awk '{print $2}')
-# ARB_DEST_DIR=$(cd "$(dirname "$ARB_DEST_DIR")"; pwd)/$(basename "$ARB_DEST_DIR")
-echo "ARB_DEST_DIR: $ARB_DEST_DIR"
 
-# if [ -z "$ARB_DEST_DIR" ]; then
-#     echo "Error: Could not find 'arb_dir' in pubspec.yaml"
-#     exit 1
-# fi
+if [ -z "$ARB_DEST_DIR" ]; then
+    echo "Error: Could not find 'arb_dir' in pubspec.yaml"
+    exit 1
+fi
 
-# # Unzip the file
-# unzip "$ZIP_SOURCE" -d "$ZIP_DEST_DIRECTORY"
+ARB_DEST_DIR=$(cd "$(dirname "$ARB_DEST_DIR")"; pwd)/$(basename "$ARB_DEST_DIR")
 
-# # Rename and move the files to the correct directory
-# find $ZIP_DEST_DIRECTORY -type f -name "intl_en.arb" | while read file; do
-#     lang=$(basename $(dirname $(dirname "$file")))
-#     new_name="intl_${lang}.arb"
-#     echo "Moving $file to $ARB_DEST_DIR/$new_name"
-#     mv "$file" "$ARB_DEST_DIR/$new_name"
-# done
+# Unzip the file
+unzip "$ZIP_SOURCE" -d "$ZIP_DEST_DIRECTORY" > /dev/null
 
-# # Create translations
-# dart run intl_utils:generate
+# Rename and move the files to the correct directory
+find $ZIP_DEST_DIRECTORY -type f -name "intl_en.arb" | while read file; do
+    lang=$(basename $(dirname $(dirname "$file")))
+    new_name="intl_${lang}.arb"
+    echo "Moving $file to $ARB_DEST_DIR/$new_name"
+    mv "$file" "$ARB_DEST_DIR/$new_name"
+done
 
-# # Clean up
-# rm -rf "$ZIP_DEST_DIRECTORY"
+# Create translations
+dart run intl_utils:generate
+
+# Clean up
+rm -rf "$ZIP_DEST_DIRECTORY"
